f4197617a1eed7b3601630bdf788473414930ffd
sebtn <sdebedout@gmail.com>
2017-05-24 20:21:44 -0400

    table resizing

--- src/Components/HarViewer.jsx
+++ src/Components/HarViewer.jsx
@@ -3,6 +3,7 @@ import _ from 'lodash'
 import {Row, Col, PageHeader, Button, ButtonGroup, Input} from 'react-bootstrap'
 import FixedDataTable from 'fixed-data-table-2'
 import 'fixed-data-table-2/dist/fixed-data-table.css'
+'use strict'
 
 const {Table, Column, Cell} = FixedDataTable
 
@@ -10,6 +11,7 @@ export default class HarViewer extends Component {
 	constructor() {
 		super() 
 		this.state = {
+			isColumnResizing: false,
 			columnWidths: {
 				url:500,
 				size:200,
@@ -19,15 +21,26 @@ export default class HarViewer extends Component {
 			tableHeight: 500
 		}
 		this._getEntry = this._getEntry.bind(this)
+		this._onColumnResized = this._onColumnResized.bind(this)
 	}
 
-	/*_________________
-
-	 METHODS CUTSOM
-	 _________________*/
+// ___________________________________________________________________
 
 	_getEntry(index) {
-		return this.props.entries[index]
+		return  this.props.entries[index] 
+	}
+
+	_onColumnResized(newColumnWidth, columnKey) { 
+		let columnWidths = this.state.columnWidths
+		// Select the col by key, then  the new col 
+		// width can be set anything, this case 
+		// is called newColumnWidth... could be x
+		columnWidths[columnKey] = newColumnWidth
+		this.setState({columnWidths: columnWidths, isResizable:false})
+	}
+
+	componentDidMount() {
+		
 	}
 
 	render() {
@@ -41,22 +54,28 @@ export default class HarViewer extends Component {
 					</Row>			
 				</div>
 
-				<Row className="container-fluid">
+				<Row className="container">
 					<Col sm={12}>
-						<Table rowsCount={this.props.entries.length}
-										width={this.state.tableWidth}
-										headerHeight={35}
+						<Table  rowsCount={this.props.entries.length}
+										width={this.state.tableWidth} 
+										headerHeight={35} 
 										height={this.state.tableHeight}
 										rowHeight={30}
-										rowGetter={this._getEntry}>
+										rowGetter={this._getEntry}
+										isColumnResizing={this.state.isColumnResizing}
+										onColumnResizeEndCallback={this._onColumnResized}
+										>
 							<Column header="Url" 
+											columnKey="url"
 											width={this.state.columnWidths.url}
 											isResizable={true}
 											flexGrow={null} />							
-							<Column  header="Size" 
+							<Column header="Size" 
+											columnKey="size"
 											width={this.state.columnWidths.size}
 											isResizable={true} />							
 						<Column   header="TimeLine"
+											columnKey="time"
 											width={this.state.columnWidths.time}
 											minWidth={200}
 											isResizable={true} />
@@ -68,7 +87,6 @@ export default class HarViewer extends Component {
 		)
 	}
 }
-
 
 HarViewer.defaultProps = {
 	entries: []
